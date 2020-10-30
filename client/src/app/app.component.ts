d649476190c2bdbb17135b8d52096b4e9b724a54
Robert.Bauld <Robert.Bauld@gmail.com>
2020-10-30 08:36:25 +0000

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +15,16 @@ export class AppComponent implements OnInit {
 
   users: any;
 
-  constructor(private http: HttpClient) {  }
+  constructor(private accountService: AccountService) {  }
 
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers()    {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-      console.log(this.users);
-    }, error => { console.log(error); });
-
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
+
 
 }
